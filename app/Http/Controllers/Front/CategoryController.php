9d231f26fcc87e6e9ec367f3b9c732e480f01f57
Hoang Minh <hoangminhzxzx@gmail.com>
2021-11-07 20:17:01 +0700

    frontend - update filter product

--- app/Http/Controllers/Front/CategoryController.php
+++ app/Http/Controllers/Front/CategoryController.php
@@ -5,23 +5,29 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use App\Model\Category;
 use App\Model\Product;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\Paginator;
-use Matrix\Builder;
 
 class CategoryController extends Controller
 {
-    public function listProductFollowCategory($slug)
+    public function listProductFollowCategory(Request $request,$slug)
     {
+//        dd($request->input());
         $category = Category::query()->where('slug', '=', $slug)->first();
         if ($category) {
+            $filter_category = intval($request->query('filter_category'));
+            $filter_range_price = json_decode($request->query('filter_range_price'));
+
             $category_title = $category->title;
             $list_category = [];
             $category_childs = $category->childs;
             $list_category[] = $category->id;
+            $list_category_filter_ids = [];
             if ($category_childs->count() > 0) {
                 foreach ($category_childs as $category_child) {
                     $list_category[] = $category_child->id;
+                    $list_category_filter_ids[] = $category_child->id;
                 }
             }
 
@@ -29,16 +35,25 @@ class CategoryController extends Controller
             if (isset($_GET['limit']) && $_GET['limit']) {
                 $limit = $_GET['limit'];
             }
-
             $orderBy = 'asc';
             if (isset($_GET['orderBy']) && $_GET['orderBy']) {
                 $orderBy = $_GET['orderBy'];
             }
             $list_product = Product::query()
+                ->when($filter_category, function (\Illuminate\Database\Eloquent\Builder $query) use ($filter_category) {
+                    return $query->where('category_id', '=', $filter_category);
+                })
+                ->when($filter_range_price, function (Builder $query) use ($filter_range_price) {
+                    return $query->where('price', '>=', $filter_range_price[0])->where('price', '<', $filter_range_price[1]);
+                })
                 ->whereIn('category_id', $list_category)
                 ->where('is_publish', '=', 1)
                 ->orderBy('price', $orderBy)
                 ->paginate($limit);
+
+            if (!empty($list_category_filter_ids)) {
+                $list_category_filter = Category::query()->whereIn('id', $list_category_filter_ids)->get();
+            }
 
             return view('front.category.list_product',
                 [
@@ -46,7 +61,10 @@ class CategoryController extends Controller
                     'category_title' => $category_title,
                     'slug' => $slug,
                     'orderBy' => $orderBy,
-                    'limit' => $limit
+                    'limit' => $limit,
+                    'list_category_filter' => $list_category_filter,
+                    'filter_category' => $filter_category,
+                    'filter_range_price' => $request->query('filter_range_price'),
                 ]
             );
 //            }
--- resources/views/front/category/list_product.blade.php
+++ resources/views/front/category/list_product.blade.php
@@ -3,52 +3,101 @@
     {{ $category_title }}
 @endsection
 @section('content')
-    <div class="small-container wp-list-product">
-        <div class="row row-2" style="padding-left: 10px;">
-            <h2>{{ $category_title }} của Diana Authentic</h2>
-            <form action="{{ route('client.category.list.product', ['slug' => $slug]) }}" id="formFilterProduct">
-{{--                @csrf--}}
-                <select name="orderBy" style="border: 1px solid #ccc;" onchange="orderProduct(this)" data-category-slug="{{ $slug }}">
-{{--                    <option value="">Mặc định</option>--}}
-                    <option value="asc" @if(isset($orderBy) && $orderBy == 'asc') selected @endif>Giá tăng dần</option>
-                    <option value="desc" @if(isset($orderBy) && $orderBy == 'desc') selected @endif>Giá giảm dần</option>
-                </select>
-
-                <select name="limit" style="border: 1px solid #ccc;" onchange="orderProduct(this)" data-category-slug="{{ $slug }}">
-                    {{--                    <option value="">Mặc định</option>--}}
-                    <option value="4" @if(isset($limit) && $limit == 4) selected @endif>4</option>
-                    <option value="8" @if(isset($limit) && $limit == 8) selected @endif>8</option>
-                    <option value="12" @if(isset($limit) && $limit == 12) selected @endif>12</option>
-                    <option value="16" @if(isset($limit) && $limit == 16) selected @endif>16</option>
-                    <option value="20" @if(isset($limit) && $limit == 20) selected @endif>20</option>
-                </select>
-            </form>
-        </div>
-        <div class="row" style="justify-content: unset;">
-            @foreach($list_product as $product)
-            <div class="col-4 box-item-product">
-                <a class="wp-img" href="{{ route('client.product.detail', ['slug' => $product->slug]) }}"><img src="{{ url($product->thumbnail) }}" alt=""></a>
-                <div class="box-info-desc">
-                    <h5>{{ $product->title }}</h5>
-                    <p>{{ number_format($product->price, 0, '.', '.') }}đ</p>
-                </div>
-{{--                <div class="rating">--}}
-{{--                    <i class="fa fa-star" aria-hidden="true"></i>--}}
-{{--                    <i class="fa fa-star" aria-hidden="true"></i>--}}
-{{--                    <i class="fa fa-star" aria-hidden="true"></i>--}}
-{{--                    <i class="fa fa-star" aria-hidden="true"></i>--}}
-{{--                    <i class="fa fa-star-o" aria-hidden="true"></i>--}}
-{{--                </div>--}}
+    <div class="container">
+        <div class="wp-list-product">
+            <div class="wp-filter-product">
+                <form action="{{ route('client.category.list.product', ['slug' => $slug]) }}" id="filterForm" method="GET">
+                    <a class="clean-filter">Bỏ lọc</a>
+                    <div class="box-filter">
+                        <h5>Theo thương hiệu</h5>
+                        @if(isset($list_category_filter) && $list_category_filter)
+                            @foreach($list_category_filter as $k=>$category_fillter)
+                                <div class="single-filter">
+                                    <input type="radio" @if(isset($filter_category) && $filter_category == $category_fillter->id) checked @endif class="filter-input remove-class-default" name="filter_category" value="{{ $category_fillter->id }}" id="filter_category_{{ $k + 1 }}">
+                                    <label for="filter_category_{{ $k + 1 }}" class="filter-title">{{ $category_fillter->title }}</label>
+                                </div>
+                            @endforeach
+                        @endif
+                    </div>
+                    <div class="box-filter">
+                        <h5>Theo khoảng giá</h5>
+                        <div class="single-filter">
+                            <input type="radio" class="filter-input remove-class-default" @if($filter_range_price == '[1000000, 2000000]') checked @endif name="filter_range_price" value="[1000000, 2000000]" id="filter_range_price_1">
+                            <label for="filter_range_price_1" class="filter-title">1M - 2M</label>
+                        </div>
+                        <div class="single-filter">
+                            <input type="radio" class="filter-input remove-class-default" @if($filter_range_price == '[2000000, 4000000]') checked @endif name="filter_range_price" value="[2000000, 4000000]" id="filter_range_price_2">
+                            <label for="filter_range_price_2" class="filter-title">2M - 4M</label>
+                        </div>
+                    </div>
+                    <input type="submit" class="btn-submit-filter" value="Áp dụng" >
+                </form>
             </div>
-            @endforeach
+            <div class="small-container remove-litle-small">
+                <div class="row row-2" style="padding-left: 10px;">
+                    <h2>{{ $category_title }} của Diana Authentic</h2>
+                    <form action="{{ route('client.category.list.product', ['slug' => $slug]) }}" id="formFilterProduct">
+                        {{--                @csrf--}}
+                        <select name="orderBy" style="border: 1px solid #ccc;" onchange="orderProduct(this)" data-category-slug="{{ $slug }}">
+                            {{--                    <option value="">Mặc định</option>--}}
+                            <option value="asc" @if(isset($orderBy) && $orderBy == 'asc') selected @endif>Giá tăng dần</option>
+                            <option value="desc" @if(isset($orderBy) && $orderBy == 'desc') selected @endif>Giá giảm dần</option>
+                        </select>
+
+                        <select name="limit" style="border: 1px solid #ccc;" onchange="orderProduct(this)" data-category-slug="{{ $slug }}">
+                            {{--                    <option value="">Mặc định</option>--}}
+                            <option value="4" @if(isset($limit) && $limit == 4) selected @endif>4</option>
+                            <option value="8" @if(isset($limit) && $limit == 8) selected @endif>8</option>
+                            <option value="12" @if(isset($limit) && $limit == 12) selected @endif>12</option>
+                            <option value="16" @if(isset($limit) && $limit == 16) selected @endif>16</option>
+                            <option value="20" @if(isset($limit) && $limit == 20) selected @endif>20</option>
+                        </select>
+                    </form>
+                </div>
+                <div class="row" style="justify-content: unset;">
+                    @foreach($list_product as $product)
+                        <div class="col-4 box-item-product">
+                            <a class="wp-img" href="{{ route('client.product.detail', ['slug' => $product->slug]) }}"><img src="{{ url($product->thumbnail) }}" alt=""></a>
+                            <div class="box-info-desc">
+                                <h5>{{ $product->title }}</h5>
+                                <p>{{ number_format($product->price, 0, '.', '.') }}đ</p>
+                            </div>
+                            {{--                <div class="rating">--}}
+                            {{--                    <i class="fa fa-star" aria-hidden="true"></i>--}}
+                            {{--                    <i class="fa fa-star" aria-hidden="true"></i>--}}
+                            {{--                    <i class="fa fa-star" aria-hidden="true"></i>--}}
+                            {{--                    <i class="fa fa-star" aria-hidden="true"></i>--}}
+                            {{--                    <i class="fa fa-star-o" aria-hidden="true"></i>--}}
+                            {{--                </div>--}}
+                        </div>
+                    @endforeach
+                </div>
+                {{ $list_product->render('vendor.pagination.bootstrap-4') }}
+                {{--        <div class="page-btn">--}}
+                {{--            <span>1</span>--}}
+                {{--            <span>2</span>--}}
+                {{--            <span>3</span>--}}
+                {{--            <span>4</span>--}}
+                {{--            <span>&#8594;</span>--}}
+                {{--        </div>--}}
+            </div>
         </div>
-        {{ $list_product->render('vendor.pagination.bootstrap-4') }}
-{{--        <div class="page-btn">--}}
-{{--            <span>1</span>--}}
-{{--            <span>2</span>--}}
-{{--            <span>3</span>--}}
-{{--            <span>4</span>--}}
-{{--            <span>&#8594;</span>--}}
-{{--        </div>--}}
     </div>
+@endsection
+
+@section('scripts')
+    <script>
+        $(document).ready(function () {
+            $('.clean-filter').click(function () {
+                let list_input_filter = $(".filter-input");
+                list_input_filter.each(function (index,item) {
+                    // console.log(item);
+                    if (item.hasAttribute('checked')) {
+                        item.removeAttribute('checked');
+                    }
+                })
+                $("#filterForm").submit();
+            })
+        })
+    </script>
 @endsection
